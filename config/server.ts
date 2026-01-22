2da61ac2d1c695fa253babcca944c44b3ec0dcf4
colaberry016 <saitejesh@colaberry.com>
2026-01-22 18:50:17 +0530

    Test commit for debug

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
-  host: '0.0.0.0',
+  host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env('APP_KEYS').split(','),
+    keys: env.array('APP_KEYS'),
   },
 });
